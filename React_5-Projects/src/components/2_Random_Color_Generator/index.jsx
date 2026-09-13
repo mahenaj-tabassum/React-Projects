f2025ed658a92e8b8627e12158eb01ab138f7fbe
mahenajT <mahenaj.dev@gmail.com>
2026-09-13 06:27:18 +0600

    Heading style change

--- React_5-Projects/src/components/2_Random_Color_Generator/index.jsx
+++ React_5-Projects/src/components/2_Random_Color_Generator/index.jsx
@@ -64,7 +64,7 @@ const Random_Color_Generator = () => {
       style={{ backgroundColor: color }}
       className={`gap-20 flex-col min-h-screen w-screen flex items-center justify-center text-[#C9B6E4]`}
     >
-      <h1 className="bg-[#2e2e41]/60 backdrop-blur-2xl text-center px-5 py-4 rounded-lg text-6xl">
+      <h1 className="bg-[#2e2e41] text-center px-5 py-4 rounded-lg text-6xl">
         {typeOfColor === "hex" ? "Hex Color:" : "RGB Color:"} {color}
       </h1>
       <div className="flex md:flex-row flex-col gap-5 items-center">
